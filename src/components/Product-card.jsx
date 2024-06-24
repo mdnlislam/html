2353314eb4150335456974e8d44b58d8226b39c7
md.nurul <nurulislam8433@gmail.com>
2024-06-24 13:15:22 +0600

    some modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -2,66 +2,26 @@ import reactLogo from "../assets/react.svg";
 function ProductCard(){
     return <>
     
-    
-    <h2 className="heading heading2">product Card</h2>
+    {/* new project */}
+    <h2>Card Flip with Text</h2>
+    <h3>Hover over the image below:</h3>
+    <div className="flip-card">
+        <div className="flip-card-inner">
+            <div className="flip-card-front">
+                <img src="public/img/Rakib.jpg" alt="" />
 
-    <div className="div div1">
-        <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png" alt="Denim jeans" />
-        <h2>Tailored Jeans</h2>
-         <p>$19.99</p>
-         <p>Some text about the jeans.Super slim
-            and comfy lorem ipsum lorem jeansum.lorem
-            jeamsum denim lorem jeansum
-         </p>
-         <p className="paragraph paragraph3"><button>Add to Cart</button></p>
-    </div>
+            </div>
+            <div className="flip-card-back">
+                <h1>John Doe</h1>
+                <p>Architect & Engineer</p>
+                <p>We love that guy</p>
 
+            </div>
 
-    {/* Now User Profile Card */}
-
-    <h2 className="heading heading2">User Profile Card</h2>
-
-    <div className="div2">
-       <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png
-" alt="john Doe" />
-  <h2>John Doe</h2>
-  <p className="title">CEO & Founder,Example</p>
-  <p>Harvard University</p>
-
-  <div className="div div3">
-
-    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-    <a href="#"><i class="fa-brands fa-twitter"></i></a> 
-    <a href="#"><i class="fa-brands fa-linkedin"></i></a> 
-    <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
-
-     </div>
-    {/* <a href="#">
-        <img className="img img1" src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1717503717/social%20icons/a1br2w2rvigka8caokd9.png
-" alt="" />
-    </a>
-    <a href="#">
-        <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1717503771/social%20icons/fyllcitxotvo9ejupfce.png
-" alt="" />
-    </a>
-    <a href="#">
-        <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1717503915/social%20icons/qvlaqsw56ug6mmdftin0.png
-
-" alt="" />
-    </a>
-    <a href="#">
-        <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1717503629/social%20icons/pe2obv89kgdyiyqs8rv2.png
-" alt="" />
-    </a>
-     <p className="paragraph paragraph4"><button>Contact</button></p> */}
-     <p className="paragraph paragraph4"><button>Contact</button></p>
- 
+        </div>
 
     </div>
-    
-    
-    
-    
+   
     
     
     
