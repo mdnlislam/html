0ed4fe2ca0d5d7d8ec270f542b648006e8d450d7
md.nurul <nurulislam8433@gmail.com>
2024-06-26 12:11:26 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -2,30 +2,18 @@ import reactLogo from "../assets/react.svg";
 function ProductCard(){
     return <>
     
-    {/* new project */}
-    <h2>Card Flip with Text</h2>
-    <h3>Hover over the image below:</h3>
-    <div className="flip-card">
-        <div className="flip-card-inner">
-            <div className="flip-card-front">
-                <img src="public/img/Rakib.jpg" alt="" />
-
-            </div>
-            <div className="flip-card-back">
-                <h1>John Doe</h1>
-                <p>Architect & Engineer</p>
-                <p>We love that guy</p>
-
-            </div>
-
-        </div>
-
-    </div>
+        {/* new project */}
    
+     <div>
+<h2>
+     CSS Links
+</h2>
+  
+   <a className="anchor anchor" href="http://www.google.com">GOOGLE</a>
+                 
+     </div>
     
-    
-    
-    Rakib</>
+     </>
 
 }
 export default ProductCard
