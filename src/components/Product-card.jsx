f60575c2e58c8f1ee8004d7ccfa18ed6a516ac2a
md.nurul <nurulislam8433@gmail.com>
2024-06-30 10:51:00 +0600

    modification this item

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -4,28 +4,42 @@ function ProductCard(){
     
         {/* new project */}
 
-            <div className="container">
+            <div className="body">
 
-                 
-              <div className="Box">
-                <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png" alt="" />
-
-              </div>
-              <div className="Box">
-                <img src="public/img/Rakib.jpg" alt="" />
-
-              </div>
-
-              <div className="Box">
-                <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png" alt="" />
-
-              </div>
-
-              <div className="Box">
-                <img src="public/img/Rakib.jpg" alt="" />
-
-              </div>
-
+               <div className="container">
+                <div className="card">
+                    <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png
+" alt="" />
+                        <div className="intro">
+                        <h2>LINK THIS</h2>
+                        <p>bangladesh is a small county. Bangladesh is a Beautiful</p>
+                        </div>
+                        
+                </div>
+                <div className="card">
+                    <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png
+" alt="" />
+                        <div className="intro">
+                        <h2>LINK THIS</h2>
+                        <p>bangladesh is a small county. Bangladesh is a Beautiful</p>
+                        </div>
+                        
+                </div>
+                <div className="card">
+                    <img src="https://res.cloudinary.com/dnhpk5q85/image/upload/v1716914477/social%20icons/lvvx1nqo7wwabm9t9wog.png
+" alt="" />
+                        <div className="intro">
+                        <h2>LINK THIS</h2>
+                        <p>bangladesh is a small county. Bangladesh is a Beautiful</p>
+                        </div>
+                        
+                </div>
+                
+                
+                
+                
+                </div>  
+            
 
 
 
