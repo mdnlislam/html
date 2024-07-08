436230d0c3225a4520898fa75032682380e01e45
md.nurul <nurulislam8433@gmail.com>
2024-07-08 23:13:49 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -23,7 +23,30 @@ function ProductCard() {
                You can use the overflow property
                You can use the overflow property
                You can use the overflow property
-               </div>     
+               </div>    
+
+
+               <h1>The Display property</h1>
+               <h2>Display:Inline</h2>
+               <div>all the best of bangladesh
+               all the <span className="A">Very</span> best of bangladesh
+               all the best of bangladesh
+               all the <span className="A">Nice</span> best of bangladesh
+               all the best of bangladesh </div>
+                
+                <h2>Display:Inline-Block</h2>
+              <div>  all the best of bangladesh
+                all the <span className="B">BIG</span> best of bangladesh
+                all the best <span className="B">Small</span> of bangladesh
+                all the best of bangladesh </div>
+
+              <h2>Display:Block</h2>
+              <div>all the best of bangladesh
+              all the best <span className="D">Good</span> of bangladesh
+              all the best of bangladesh
+              all the best <span className="D">Main</span> of bangladesh
+              all the best of bangladesh
+              </div>
 
       </div>
 
