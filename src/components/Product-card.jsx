cc780824437aeb9a525ebd885f24df28d3a6c9cb
md.nurul <nurulislam8433@gmail.com>
2024-07-09 18:31:20 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -6,47 +6,43 @@ function ProductCard() {
 
       <div className="main-card">
 
-         
-              <h2>Overflow:Scroll</h2>    
-              <p> Setting the overflow value Setting the overflow value
-              Setting the overflow value
-              Setting the overflow value
-              Setting the overflow value
-              Setting the overflow value
-              Setting the overflow value
-              Setting the overflow value </p> 
-              <div className="text"> 
-               You can use the overflow property
-               You can use the overflow property
-               You can use the overflow property
-               You can use the overflow property
-               You can use the overflow property
-               You can use the overflow property
-               You can use the overflow property
-               </div>    
-
-
-               <h1>The Display property</h1>
-               <h2>Display:Inline</h2>
-               <div>all the best of bangladesh
-               all the <span className="A">Very</span> best of bangladesh
-               all the best of bangladesh
-               all the <span className="A">Nice</span> best of bangladesh
-               all the best of bangladesh </div>
-                
-                <h2>Display:Inline-Block</h2>
-              <div>  all the best of bangladesh
-                all the <span className="B">BIG</span> best of bangladesh
-                all the best <span className="B">Small</span> of bangladesh
-                all the best of bangladesh </div>
-
-              <h2>Display:Block</h2>
-              <div>all the best of bangladesh
-              all the best <span className="D">Good</span> of bangladesh
-              all the best of bangladesh
-              all the best <span className="D">Main</span> of bangladesh
-              all the best of bangladesh
-              </div>
+                  <h2>Overflow:Hidden</h2>
+                   
+                   <div className="text">
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                   </div>
+                   <div className="font">
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                   </div>
+                   <br />
+                   <br />
+                   <br />
+                   
+                   <div className="back">
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                     Bangladesh is a beautyfull county
+                   </div>
+             
 
       </div>
 
