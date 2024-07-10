eb2e7ca6a836c725412250edc3756c14c38d46c2
md.nurul <nurulislam8433@gmail.com>
2024-07-10 20:48:10 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -6,144 +6,22 @@ function ProductCard() {
 
       <div className="main-card">
 
-                  {/* <h2>Overflow:Hidden</h2>
-                   
-                   <div className="text">
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                   </div>
-                   <div className="font">
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                   </div>
-                   <br />
-                   <br />
-                   <br />
+           
+           <h2>Dropdown Menu</h2>
+           <div className="dropdown">
+            <button className="dropbtn">Dropdown</button>
 
-                   <div className="back">
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                     Bangladesh is a beautyfull county
-                   </div>
+           </div>
+           <div className="Dropdown-content">
+            <a className="A" href="#">Open</a>
+            <a className="A" href="#">Close</a>
+            <a className="A" href="#">About</a>
+            <a className="A" href="#">Ok</a>
 
-
-                   <h2>Flexible Boxes</h2>
-
-                   <div className="display-flex">
-
-                     <div>1</div>
-                     <div>2</div>
-                     <div>3</div>
-
-                   </div>
-
-                   <div className="display-inline-flex">
-                          <div className="A">1</div>
-                          <div className="A">2</div>
-                          <div className="A">3</div>
-                   </div>
- */}
-
-                   {/* <p> A menu icon</p>
-
-                   <div className="A"></div>
-                   <div className="A"></div>
-                   <div className="A"></div>
-              */}
-
-{/* 
-
-                 <p>Click on the Menu Icon to transform it to "X"</p>   
-
-                 <div className="container">
-                    <div className="A1"></div>
-                    <div className="A2"></div>
-                    <div className="A3"></div>
-
-                 </div>
-                 {/* <script>
-               //    function myfunction(x) { */}
-               {/* //       XMLDocument.classList.toggle("")
-               //    }
-               //   </script> */} */
-
-                
-{/* <i class="fa-thin fa-caret-down"></i> */}
-
-               <div className="navbar">
-                  <a href="#home">Home</a>
-                  <a href="#news">News</a>
-                  <div className="dropdown">
-                  <button className="drobbtn">
-                           Dropdown
-                   <i className="fa-thin fa-caret-down"></i>
-                  </button>
-                  <div className="dropdown-content">
-                   <div className="header">
-                  <h2>Mega Mnue</h2>
-
-                              </div>
-          <div className="row">
-
-            <div className="column">
-               <h3>Category 1</h3>
-               <a href="#">Link 1</a>
-               <a href="#">Link 2</a>
-               <a href="#">Link 3</a>
-
-            </div>
-
-            <div className="column">
-               <h3>Category 2</h3>
-               <a href="#">Link 1</a>
-               <a href="#">Link 2</a>
-               <a href="#">Link 3</a>
-            </div>
-
-            <div className="column">
-               <h3>Category 3</h3>
-               <a href="#"> Link 1</a>
-               <a href="#">Link 2</a>
-               <a href="#">Link 3</a>
-
-            </div>
+           </div>
 
 
 
-                        </div>                    
-                        </div>
-                        </div>
-
-                        </div>
-             <div className="text">
-              <h3>Responsive Mega Menu(full-width 
-               dropdown in navbar)</h3> 
-               <p>Hover over the 
-                  "Dropdown"link to see the 
-                  mega manu
-               </p>
-               <p>
-                  Resize the browser window to see 
-                  Responsive effect.
-               </p>
-               </div>           
 
       </div>
 
