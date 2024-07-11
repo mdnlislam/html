ad1651ee82c34279cc4a6f657fc21ed33937e683
md.nurul <nurulislam8433@gmail.com>
2024-07-11 21:12:21 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -5,23 +5,52 @@ function ProductCard() {
       {/* new project */}
 
       <div className="main-card">
+      {/* <i class="fa-duotone fa-caret-down"></i> */}
 
-             <div className="DropDown">
-               
-                  <button className="Dropbtn">Dropdown</button>
-
-                  <div className="Dropdown-contant">
-                     <a href="#">link 1</a>
-                     <a href="#">link 2</a>
-                     <a href="#">link 3</a>
-                     <a href="#">link 4</a>
-
-                  </div>
-               
+           <div className="navbar">
+            <a href="#">HOME</a>
+            <div className="subnav">
+             <button className="subnavbtn">About <i class="fa-duotone fa-caret-down"></i></button>
+             <div className="subnav-content">
+               <a href="#">Company</a>
+               <a href="#">Team</a>
+               <a href="#">Careers</a>
 
              </div>
+            </div>
 
+               <div className="subnav">
+                  <button className="subnavbtn">Services <i class="fa-duotone fa-caret-down"></i> </button>
+                <div className="subnav-content">
+                  <a href="#">Bring</a>
+                  <a href="#">Deliver</a>
+                  <a href="#">Package</a>
+                  <a href="#">Express</a>
+                  </div>    
+               </div>
+                 
+                 <div className="subnav">
+                  <button className="subnavbtn">Partners <i class="fa-duotone fa-caret-down"></i></button>
+                     <div className="subnav-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                        <a href="#">Link 4</a>
 
+                     </div>
+                 </div>
+
+               <a href="#">Contact</a>
+
+            </div>  
+            <div className="text">
+             <h3>Subnav / dropdown menu inside a 
+               Navigation Bar
+             </h3>
+             <p>hover over the About
+               servics or partners link to see the navigation menu
+             </p>
+            </div>
 
       </div>
 
