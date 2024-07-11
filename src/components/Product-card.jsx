804a80462e09edc925589282e4e0c2556c1fe329
md.nurul <nurulislam8433@gmail.com>
2024-07-11 22:44:28 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -4,58 +4,49 @@ function ProductCard() {
 
       {/* new project */}
 
-      <div className="main-card">
-      {/* <i class="fa-duotone fa-caret-down"></i> */}
+     <div className="card-menu">
 
-           <div className="navbar">
-            <a href="#">HOME</a>
-            <div className="subnav">
-             <button className="subnavbtn">About <i class="fa-duotone fa-caret-down"></i></button>
-             <div className="subnav-content">
-               <a href="#">Company</a>
-               <a href="#">Team</a>
-               <a href="#">Careers</a>
+           <div className="scrollmenu">
+            <a href="#">Home</a>
+            <a href="#">News</a>
+            <a href="#">Contact</a>
+            <a href="#">About</a>
+            <a href="#">Support</a>
+            <a href="#">Blog</a>
+            <a href="#">Tools</a>
+            <a href="#">Base</a>
+            <a href="#">Custom</a>
+            <a href="#">More</a>
+            <a href="#">Logo</a>
+            <a href="#">Friends</a>
+            <a href="#">Partners</a>
+            <a href="#">People</a>
+            <a href="#">Work</a>
+            <a href="#">Back</a>
+            <a href="#">Lonk</a>
 
-             </div>
-            </div>
 
-               <div className="subnav">
-                  <button className="subnavbtn">Services <i class="fa-duotone fa-caret-down"></i> </button>
-                <div className="subnav-content">
-                  <a href="#">Bring</a>
-                  <a href="#">Deliver</a>
-                  <a href="#">Package</a>
-                  <a href="#">Express</a>
-                  </div>    
-               </div>
-                 
-                 <div className="subnav">
-                  <button className="subnavbtn">Partners <i class="fa-duotone fa-caret-down"></i></button>
-                     <div className="subnav-content">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                        <a href="#">Link 4</a>
-
-                     </div>
-                 </div>
-
-               <a href="#">Contact</a>
-
-            </div>  
-            <div className="text">
-             <h3>Subnav / dropdown menu inside a 
-               Navigation Bar
-             </h3>
-             <p>hover over the About
-               servics or partners link to see the navigation menu
-             </p>
-            </div>
-
-      </div>
+           </div>
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     </div>
+            
+    
 
    </>
 
