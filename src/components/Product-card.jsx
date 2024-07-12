d1bb0d9a4f51dbd82db344add0e6df36625d0ccf
md.nurul <nurulislam8433@gmail.com>
2024-07-12 23:55:59 +0600

    modification

--- src/components/Product-card.jsx
+++ src/components/Product-card.jsx
@@ -4,32 +4,177 @@ function ProductCard() {
 
       {/* new project */}
 
-     <div className="card-menu">
+    
+         <div className="project">
+
+
+
+          <h2> Admission Office:01992887317,01968774933,01968774931,01968774927(Director),019687749030</h2>
+
+            <ul className="logo_card">
+               <li><a href="#">Jobs at EUB</a></li>
+               <li><a href="#"> IQAC</a></li>
+               <li><a href="#">i-EMS</a></li>
+            </ul>
+
+         </div>   
+
+
 
          
+         <div className="logo_area" >
+                  
+                  <img src="public/img/Eub.png" alt="" />
+                   
+                     <h2 className="heading">EUROPEAN UNIVERSITY <br /> <span className="span_3">OF</span> <span className="span_1">  BANGLADESH</span> <br /><span className="span_2">WE SHAPE YOUR DREAM</span></h2>
+                    
 
-          <div className="newproject">
+                    <ul className="Infrom_text">
+                     <li><a href="#">Emergency Contact</a></li>
+                     <li><a href="#">Degree Verification</a></li>
+                     <li><a href="#">Student Portal</a></li>
+                     <li><a href="#">Apply for Cerificate</a></li>
+                    </ul>
 
-          {/* <i class="fa-solid fa-alarm-clock"></i> */}
-
-
-          <div className="first">
-            <img src="public/img/car.jpg" alt="" />
-            <p>BERRY</p>
-            <div className="icon">
-               <a href="#">A</a>
-               <a href="#">B</a>
-               <a href="#">C</a>
-               <a href="#">D</a>
+                </div>
 
 
+                <div className="Menu_Ber">
 
-             </div>
+                  <div className="navbar">
+                     <a href="#">Home</a>
+                     <div className="subnav">
+                   <button className="subnavbtn">About</button>
+                       <div className="subnav_content">
+                          <a href="#">Humble Beginning</a>
+                          <a href="#">Messages</a>
+                          <a href="#">Certification</a>
+                          <a href="#">Board of Trustees</a>
+                          <a href="#">Mission and Vision</a>
+                          <a href="#">Special Features</a>
+                          <a href="#">Graduate Profile</a>
+                          <a href="#">Gallery</a>
+                       </div>
+                     </div>
 
-          </div>
 
-            
-          </div>
+                     <div className="subnav">
+                   <button className="subnavbtn">Academics</button>
+                       <div className="subnav_content">
+                          <a href="#">Academic policy</a>
+                          <a href="#">Rules & Regulation</a>
+                          <a href="#">Degrees offered</a>
+                          <a href="#">Credit Transfer</a>
+                          <a href="#">Examination</a>
+                          <a href="#">Academic Calender</a>
+                          
+                       </div>
+                     </div>
+
+
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Admission</button>
+                       <div className="subnav_content">
+                          <a href="#">Admission Information</a>
+                          <a href="#">Waivers and Assistance</a>
+                          <a href="#">Courses & Tuition Fees</a>
+                          <a href="#">Students Welfare Committee</a>
+                          <a href="#">Registration</a>
+                          <a href="#">Admission Office</a>
+                          
+                       </div>
+                     </div>
+
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Faculties</button>
+                       <div className="subnav_content">
+                          <a href="#">Faculty of Arts & Social Sciences</a>
+                          <a href="#">Faculty of Business Administration</a>
+                          <a href="#">Faculty of Sciences & Engineering</a>
+                          <a href="#">Faculty Members</a>
+                          
+                       </div>
+                     </div>
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Administration</button>
+                       <div className="subnav_content">
+                          <a href="#">Academic policy</a>
+                          <a href="#">Rules & Regulation</a>
+                          <a href="#">Degrees offered</a>
+                          <a href="#">Credit Transfer</a>
+                          <a href="#">Examination</a>
+                          <a href="#">Academic Calender</a>
+                          
+                       </div>
+                     </div>
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Student Corner</button>
+                       <div className="subnav_content">
+                          <a href="#">Academic policy</a>
+                          <a href="#">Rules & Regulation</a>
+                          <a href="#">Degrees offered</a>
+                          <a href="#">Credit Transfer</a>
+                          <a href="#">Examination</a>
+                          <a href="#">Academic Calender</a>
+                          
+                       </div>
+                     </div>
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Contact Us</button>
+                       <div className="subnav_content">
+                          <a href="#">Academic policy</a>
+                          <a href="#">Rules & Regulation</a>
+                          <a href="#">Degrees offered</a>
+                          <a href="#">Credit Transfer</a>
+                          <a href="#">Examination</a>
+                          <a href="#">Academic Calender</a>
+                          
+                       </div>
+                     </div>
+
+
+                     <div className="subnav">
+                   <button className="subnavbtn">Research & Journals</button>
+                       <div className="subnav_content">
+                          <a href="#">Academic policy</a>
+                          <a href="#">Rules & Regulation</a>
+                          <a href="#">Degrees offered</a>
+                          <a href="#">Credit Transfer</a>
+                          <a href="#">Examination</a>
+                          <a href="#">Academic Calender</a>
+                          
+                       </div>
+                     </div>
+
+
+
+
+
+
+
+                  </div>
+
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+                </div>
+
+        
          
 
 
@@ -47,8 +192,7 @@ function ProductCard() {
 
 
 
-     </div>
-            
+     
     
 
    </>
